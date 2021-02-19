c353158fd4071cdfc24acb1d42860ec602dc88b7
fishball926 <68767684+fishball926@users.noreply.github.com>
2021-02-19 13:34:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'id_generator',         # How you named your package folder (MyLib)
   packages = ['id_generator'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'An unique ID generator that writes to excel file (.xlsx)',   # Give a short description about your library
   author = 'Chew Jian Xun',                   # Type in your name
